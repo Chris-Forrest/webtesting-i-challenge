4d79824ded01c193e125c04b57c2a2c4b0db96b9
Chris Forrest <redrider_cr@yahoo.com>
2020-09-08 21:38:54 -0400

    repair function working and test passing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,15 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe("enhancer unit tests",() => {
+    it("repairs the item", () => {
+        expect(enhancer.repair({
+            name: "naginata",
+            enhancement: 18,
+            durability: 86,
+        })).toEqual({
+            name: "naginata",
+            enhancement: 18,
+            durability: 100,
+        })
+    })
+})
