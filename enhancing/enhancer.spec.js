e483e31dacd2cb0489fb37f642769573a2cad0de
Chris Forrest <redrider_cr@yahoo.com>
2020-09-08 22:18:33 -0400

    success function complete test working

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -12,4 +12,16 @@ describe("enhancer unit tests",() => {
             durability: 100,
         })
     })
+    it("enhances the item success", () => {
+        expect(enhancer.success({
+            name:"naginata",
+            enhancement: 18,
+            durability: 100,
+        })).toEqual({
+            name:"naginata",
+            enhancement:19,
+            durability: 100,
+        })
+
+    })
 })
